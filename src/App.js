1cc4fc24c6457a6a21fd1af3ff81d3d6619852e9
okerekechinweotito <you@example.com>
2021-09-02 05:21:32 +0100

    test commiit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Section4PricingPage from "./Reusable_Cards/FAQ/section4_pricing_page";
+
+function App() {
+  return (
+    <div className="App">
+      <Section4PricingPage />
+    </div>
+  );
+}
+
+export default App;
